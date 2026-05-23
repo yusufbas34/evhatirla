165c72c0593043383e5400d254ae415301f8a55c
yusufbas34 <63819460+yusufbas34@users.noreply.github.com>
2026-05-24 02:13:33 +0300

    Add files via upload

---
+++ lib/app.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'core/router/app_router.dart';
+import 'core/theme/app_theme.dart';
+import 'core/constants/app_constants.dart';
+
+class EvHatirlaApp extends ConsumerWidget {
+  const EvHatirlaApp({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final router = ref.watch(routerProvider);
+    return MaterialApp.router(
+      title: AppConstants.appName,
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme.light,
+      routerConfig: router,
+    );
+  }
+}
